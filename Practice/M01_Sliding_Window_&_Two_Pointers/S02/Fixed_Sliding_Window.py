ee4013f266db6ad50a9334dced56a3a44dae1a58
Alluri Spoorthi Reddy <allurispoorthireddy07@gmail.com>
2026-07-30 15:43:10 +0530

    crt

---
+++ Practice/M01_Sliding_Window_&_Two_Pointers/S02/Fixed_Sliding_Window.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def findMaxAverage(nums: List[int], k: int) -> float:
+    max_sum = float('-inf')
+    n = len(nums)
+    for i in range(n - k + 1):
+        sub_sum = 0
+        for j in range(i, i + k):
+            sub_sum += nums[j]
+        max_sum = max(max_sum, sub_sum)
+    return max_sum / k
+
+
+nums = [1, 12, -5, -6, 50, 3]
+k = 4
+print(findMaxAverage(nums, k))
