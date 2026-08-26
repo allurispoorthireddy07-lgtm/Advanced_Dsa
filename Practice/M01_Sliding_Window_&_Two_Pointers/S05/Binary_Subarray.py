f8c07e8ab40f9ed4fe838e5c60063be27586abe3
Alluri Spoorthi Reddy <allurispoorthireddy07@gmail.com>
2026-08-26 14:57:52 +0530

    crt

--- Practice/M01_Sliding_Window_&_Two_Pointers/S05/Binary_Subarray.py
+++ Practice/M01_Sliding_Window_&_Two_Pointers/S05/Binary_Subarray.py
@@ -1,40 +1,46 @@
 '''leetcode - 1493'''
 from typing import List
 
-def longestSubarray( nums: List[int]) -> int:
-    left,zero,res = 0,0,0
+
+def longestSubarray(nums: List[int]) -> int:
+    left, zero, res = 0, 0, 0
 
     for right in range(len(nums)):
         if nums[right] == 0:
             zero += 1
 
         while zero > 1:
-            if nums[left]==0:
+            if nums[left] == 0:
                 zero -= 1
-                left += 1
-        res = max(res,right-left+1 )    
-    return res - 1 
+            left += 1
+        res = max(res, right - left + 1)
+    return res - 1
+
+
 nums = [0,1,1,1,0,1,1,0,1]
-print( longestSubarray(nums)) 
+print(longestSubarray(nums))
 
 
 '''1004'''
-def longestOnes(self, nums: List[int], k: int) -> int:
-    left,zero,res = 0,0,0
+
+
+def longestOnes(nums: List[int], k: int) -> int:
+    left, zero, res = 0, 0, 0
 
     for right in range(len(nums)):
         if nums[right] == 0:
             zero += 1
 
         while zero > k:
-            if nums[left]==0:
+            if nums[left] == 0:
                 zero -= 1
             left += 1
-        res = max(res,right-left+1 )    
-    return res  
+        res = max(res, right - left + 1)
+    return res
 
-nums=[1,1,1,0,0,0,1,1,1,1,0]
+
+nums = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0]
 k = 2
-print(longestSubarray(nums))
+print(longestOnes(nums, k))
 
 '''930'''
